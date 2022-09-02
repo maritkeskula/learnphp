01e153454d278ebafbe8165eaca1ba91dbb1559f
Marit Kesküla <MM-20-312@tptlive.ee>
2022-09-02 15:42:42 +0300

    add list

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-$content = file_get_contents('list.txt');
-$items = explode("\n", $content);
-array_pop($items);
+$json = file_get_contents('posts.json');
+$posts = json_decode($json, true);
 ?>
 
 <!DOCTYPE html>
@@ -11,19 +10,29 @@ array_pop($items);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
 </head>
 <body>
-    <form action="/add.php" method="POST">
-        <input type="text" name="newitem">
-        <input type="submit" value="Add">
-    </form>
-        <ul>
+    <div class="container">
+    <a class="button is-primary" href = "/new.php">New post</a>
             <?php
-                foreach($items as $item) : ?>
-                <li>
-                <?= $item ?>
-                </li>
+                foreach($posts as $post) : ?>
+                <div class="card">
+  <header class="card-header">
+    <p class="card-header-title">
+      <?= $post['title']?>
+    </p>
+  </header>
+  <div class="card-content">
+    <div class="content">
+    <?= $post['body']?>
+    </div>
+  </div>
+  <footer class="card-footer">
+    <a href="#" class="card-footer-item">Read more</a>
+  </footer>
+</div>
             <?php endforeach; ?>
-        </ul>
+    </div>
 </body>
 </html>
